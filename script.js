4aea09f817c097d0fd3619ed73215a22190657d2
Henrik Akselsen <henrik.akselsen@gmail.com>
2013-01-14 14:23:23 +0100

    Added route for rendered campaigns that will be used for screenshot

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {  
+    var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);  
+    var circle = paper.circle(100, 100, 80);  
+
+
+    var t = paper.text(50, 50, "Raphaël\nkicks\nbutt!");
+
+}  
+
